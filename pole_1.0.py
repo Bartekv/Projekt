efd5d33c6e4273015b4dfc9ab215c71bec0eac94
Bartekv <bartlomiejostrowski90@gmail.com>
2023-02-25 18:30:32 +0100

    pole_1.0.py
    git commit -mpole_1.0.py

---
+++ pole_1.0.py
@@ -0,0 +1,10 @@
+# Program napisany przez Bartłomieja Ostrowskiego v.1.0
+# Program obliczający pole prostokąta
+
+
+dlugosc = float(input("Podaj długość boku a: "))
+szerokosc = float(input("Podaj długość boku b: "))
+
+pole = dlugosc * szerokosc
+
+print("Pole prostokąta wynosi: ", pole)
