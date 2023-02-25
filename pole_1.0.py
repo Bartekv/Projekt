1bd48d216cab140fd368b9d6cc5a9b335bcf4912
Bartłomiej Ostrowski <bartlomiejostrowski90@gmail,com>
2023-02-25 23:08:04 +0100

    aktualizacja do wersji v1.1

--- pole_1.0.py
+++ pole_1.0.py
@@ -1,10 +1,17 @@
-# Program napisany przez Bartłomieja Ostrowskiego v.1.0
+ # Program edytowany przez autora Bartłomieja Ostrowskiego v.1.1
 # Program obliczający pole prostokąta
 
 
-dlugosc = float(input("Podaj długość boku a: "))
-szerokosc = float(input("Podaj długość boku b: "))
+# Program obliczający pole prostokąta z użyciem pętli while
 
-pole = dlugosc * szerokosc
+while True:
+    dlugosc = float(input("Podaj długość boku a: "))
+    szerokosc = float(input("Podaj długość boku b: "))
 
-print("Pole prostokąta wynosi: ", pole)
+    pole = dlugosc * szerokosc
+
+    print("Pole prostokąta wynosi: ", pole)
+
+    odp = input("Czy chcesz kontynuować (t/n)? ")
+    if odp.lower() == "n":
+        break
